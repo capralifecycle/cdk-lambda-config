1d698f6939e795194672bd31a915f483c258a8bc
Joakim L. Engeset <joakim.engeset@gmail.com>
2024-11-28 11:14:55 +0100

    fix: add missing __dirname ignored by eslint

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ import {
 } from "aws-cdk-lib"
 import { Construct } from "constructs"
 import * as path from "path"
+import { fileURLToPath } from "url"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export interface LambdaConfigProps {
   /**
