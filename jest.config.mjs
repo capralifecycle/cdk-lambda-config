810816c93ff6086c95b329a3daaabd92a1a27821
Joakim L. Engeset <joakim.engeset@gmail.com>
2024-11-28 11:14:24 +0100

    fix: jest-workaround for esm

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,6 +1,15 @@
 export default {
   testMatch: ["**/*.test.ts"],
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
   },
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  extensionsToTreatAsEsm: [".ts"],
 }
